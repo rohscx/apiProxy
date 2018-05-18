785c600c8e24ab44a17726f60cfb1fdda50f870b
rohscx <rohscx@gmail.com>
2018-05-18 09:05:27 -0400

    Update configs.js

--- configs.js
+++ configs.js
@@ -4,8 +4,8 @@ module.exports =
 {
     merakiKey: '', // Enter Your API Key as server default
     merakiUrl: 'https://api.meraki.com/api/v0',
-	apicToken:'', // Enter Your API Key as server default
-	apicUrl:'https://devnetapi.cisco.com/sandbox/apic_em/api/v1',
-	primeKey: '', // Enter Your API Key as server default
-	primeUrl:'https://jsonplaceholder.typicode.com/posts/'
+    apicToken:'', // Enter Your API Key as server default
+    apicUrl:'https://devnetapi.cisco.com/sandbox/apic_em/api/v1',
+    primeKey: '', // Enter Your API Key as server default
+    primeUrl:'https://jsonplaceholder.typicode.com/posts/'
 }
