9d282cab6064e9080ebfa9c0bd35af1990b56a57
rohscx <rohscx@gmail.com>
2018-05-18 08:55:14 -0400

    Update server.js
    added support for Cisco Prime

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ Supports redirects with custom request-meraki function.
 / ****************** */
 
 
-// Environment  Variables
+// Environment  letiables
 
 // External Configuration File
-var configs = require('./configs.js');
+const configs = require('./configs.js');
 
 /* Local Configuration alternative
-var configs = {
+let configs = {
     apiKey: 'YourAPIKey',
     apiUrl: 'https://api.meraki.com/api/v0'
 };
@@ -21,24 +21,26 @@ var configs = {
 
 /* ****************** */
 
-var express = require('express');
-//var request = require('request'); // Does not properly handle Meraki redirects
-var requestMeraki = require('./request-meraki');
-var path = require('path');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const express = require('express');
+//let request = require('request'); // Does not properly handle Meraki redirects
+const requestMeraki = require('./request-meraki');
+const requestApicEM = require('./request-apicem');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var app = module.exports = express();
 
-// enable cors response for fetch compatibility.
+const app = module.exports = express();
+
+
 app.use(cors());
 
 // Development Tools
 /*
-var morgan = require('morgan');
+let morgan = require('morgan');
 app.use(morgan('dev'))
 
-var globalLog = require('global-request-logger');
+let globalLog = require('global-request-logger');
 globalLog.initialize();
 
 globalLog.on('success', function(request, response) {
@@ -54,27 +56,33 @@ globalLog.on('error', function(request, response) {
 });
 */
 
+//defines Routes
+const routesObj = {
+	meraki:'/meraki/api',
+	apicem:'/apicem/api',
+	prime:'/pi/api'
+}
 
-var jsonParser = bodyParser.json();
+let jsonParser = bodyParser.json();
 
 // API Route - Will be proxied through Meraki Dashboard API
-app.use('/meraki/api', jsonParser, function (req, res){
+app.use(routesObj.meraki, jsonParser, function (req, res){
   console.log('API request url', req.url);
   console.log('API request headers', req.headers);
   console.log('request body, ', req.body);
   
   // Use client supplied API key or default to server config.
-  var apiKey = '';
-  if('x-cisco-meraki-api-key' in req.headers){
+  let apiKey = '';
+  if('x-cisco-meraki-api-key'.toLowerCase() in req.headers){
     apiKey = req.headers['x-cisco-meraki-api-key'];
     console.log("New headers sent", apiKey );
   }else{
-    apiKey = configs.apiKey; 
+    apiKey = configs.merakiKey; 
   }
  
-  var options = {
+  let options = {
     qs: req.query,
-    url: configs.apiUrl + req.url,
+    url: configs.merakiUrl + req.url,
     method: req.method,
     body: JSON.stringify(req.body), 
     //followAllRedirects: true, // Does not work as intended with PUT,POST,DELETE (returns a [GET] on final location)
@@ -96,6 +104,7 @@ app.use('/meraki/api', jsonParser, function (req, res){
     console.log('FINAL res.body ',response.body);
 
     res.setHeader('content-type', response.headers['content-type']);
+	
     res.status(response.statusCode).send(data);
     
     
@@ -104,10 +113,10 @@ app.use('/meraki/api', jsonParser, function (req, res){
 });
 
 // API Route - Will be proxied through Apic-EM API
-app.use('/apicem/api', jsonParser, function (req, res){
+app.use(routesObj.apicem, jsonParser, function (req, res){
 	let apiToken = '';
-	if('X-Auth-Token' in req.headers){
-		apiToken = req.headers['X-Auth-Token'];
+	if('X-Auth-Token'.toLowerCase() in req.headers){
+		apiToken = req.headers['X-Auth-Token'.toLowerCase()];
 		console.log("New headers sent", apiToken );
 	}else{
     apiToken = configs.apicToken; 
@@ -124,9 +133,50 @@ app.use('/apicem/api', jsonParser, function (req, res){
 			'Content-Type': 'application/json'
 		} 
 	}
-	requestMeraki(options, function(err, response, data){
+	requestApicEM(options, function(err, response, data){
 	if(err){
 		console.log("requestAPIRC-EM err ", err)
+		res.status(response.statusCode).send({
+			message: 'err'
+		 });
+		res.send(err);
+	}
+	console.log('FINAL res.statusCode ',response.statusCode);
+	console.log('FINAL res.body ',response.body);
+
+	res.setHeader('content-type', response.headers['content-type']);
+
+	res.status(response.statusCode).send(data);
+    
+    
+  });
+})
+
+
+// API Route - Will be proxied through ciscoPrime Infranstructure
+app.use(routesObj.prime, jsonParser, function (req, res){
+	let apiToken = '';
+	if('Authorization'.toLowerCase() in req.headers){
+		apiToken = req.headers['X-Auth-Token'.toLowerCase()];
+		console.log("New headers sent", apiToken );
+	}else{
+    apiToken = configs.primeKey; 
+	}
+	
+	let options = {
+		qs: req.query,
+		url: configs.primeUrl + req.url,
+		method: req.method,
+		body: JSON.stringify(req.body), 
+		//followAllRedirects: true, // Does not work as intended with PUT,POST,DELETE (returns a [GET] on final location)
+		headers: {
+			'Authorization': apiToken,
+			'Content-Type': 'application/json'
+		} 
+	}
+	requestApicEM(options, function(err, response, data){
+	if(err){
+		console.log("requestPrime err ", err)
 		res.status(response.statusCode).send({
 			message: 'err'
 		 });
@@ -147,10 +197,10 @@ app.use('/apicem/api', jsonParser, function (req, res){
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 // Start server
-var port = process.env.PORT || 8085;
-var server = app.listen(port, () => {
+let port = process.env.PORT || 8085;
+let server = app.listen(port, () => {
   console.log('WWW Server: http://'+ server.address().address+port+'/');
-  console.log('Meraki API Client Proxy: http://'+server.address().address+ ':' +port+'/api');
-  console.log('Meraki API Endpoint: ', configs.apiUrl);
+  Object.keys(routesObj).map(data => console.log(data.toUpperCase()+' API Client Proxy: http://'+server.address().address+ ':' +port+routesObj[data]))
+  Object.keys(configs).filter(f => f.includes("Url")).map(data => console.log(data.toUpperCase()+' API Endpoint: ', configs[data]))
 });
 
